43340aa370c34d149a8858d0be1b6999f2d8d855
xiaotang66 <862115203@qq.com>
2018-01-29 16:19:01 +0800

    新增小图标

---
+++ client/component/index/xheader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+class Xindex extends React.Component {
+	constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+         <div>
+	         <header>
+	          
+	         </header>
+          </div>
+    )
+  }
+
+}
+export default Xindex;
