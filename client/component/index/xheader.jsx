a68175e282125544117029b676a60ee23659616d
xiaotang66 <862115203@qq.com>
2018-01-29 18:00:59 +0800

    配置小图标

--- client/component/index/xheader.jsx
+++ client/component/index/xheader.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import "../../css/xindex.css";
+import "../../iconfont/iconfont.css";
 class Xindex extends React.Component {
 	constructor(props) {
     super(props);
   }
   render(){
     return(
-         <div>
-	         <header>
-	          
+         <div >
+	         <header className="wheader">
+	           <div>
+	              <i className="iconfont icon-caidanlan"></i>
+	           </div>
 	         </header>
           </div>
     )
