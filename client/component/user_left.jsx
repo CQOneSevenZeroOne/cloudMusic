696242bfbd0540affbd630b2c9c23f0c65341553
chc <1428524567@qq.com>
2018-01-31 17:35:30 +0800

    新增组件

--- client/component/user_left.jsx
+++ client/component/user_left.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../css/user_left.css'
-import imgsrc from '../images/c_head.jpg'
+import imgsrc from '../images/c_head.jpg';
+import '../iconfont/iconfont.css'
+import $ from 'jquery'
 class Xuser_left extends React.Component{
 	constructor(props){
 		super(props)
 		this.state = {
-			
+			name:'陨落星际',
+			level:'Lv.8',
+			bool:true,
+			state:'签到'
 		}
+		this.qiandao = this.qiandao.bind(this)
 	}
 	render(){
 		return (
@@ -15,21 +21,61 @@ class Xuser_left extends React.Component{
 					<img src={imgsrc} className="c_tou"/>
 					<div className="c_box">
 						<div className="c_uname">
-							<span>陨落星际</span>
-							<i>Lv.8</i>
+							<span>{this.state.name}</span>
+							<i>{this.state.level}</i>
 						</div>
-						<div className="qiandao">
-							签到
+						<div onClick={this.qiandao} className="qiandao">
+							{this.state.state}
 						</div>
 					</div>
 				</header>
 				<ul>
 					<li>
-						
+						<i className="iconfont icon-buoumaotubiao48"></i>
+						<a>我的消息</a>
+					</li>
+					<li>
+						<i className="iconfont icon-yinle101"></i>
+						<a>在线听歌免流量</a>
+					</li>
+					<li>
+						<i className="iconfont icon-qunzu"></i>
+						<a>我的好友</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<i className="iconfont icon-qunzu"></i>
+						<a>我的好友</a>
+					</li>
+					<li>
+						<i className="iconfont icon-music"></i>
+						<a>听歌识曲</a>
 					</li>
 				</ul>
 			</div>
 		)
+	}
+	qiandao(){
+		this.setState({
+			state:'已签到',
+			bool:false
+		})
+	}
+	componentDidMount(){
+		console.log(this)
+	}
+	componentDidUpdate(){
+		//show  or   hide
+		if(this.state.bool==true){
+			$('#c_left').animate({
+				left:'0'
+			},500)
+		}else{
+			$('#c_left').animate({
+				left:'-82%'
+			},500)
+		}
 	}
 }
 export default Xuser_left;
