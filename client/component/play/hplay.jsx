01a9da56da75b7795c461fbe234b8ce43f9617b6
hch <577031133@qq.com>
2018-01-31 19:14:57 +0800

    3

--- client/component/play/hplay.jsx
+++ client/component/play/hplay.jsx
@@ -1,11 +1,89 @@
 import React from 'react';
+import $ from 'jquery'
+
 import '../../css/hplay.css'
 import '../../iconfont/iconfont.css'
-class Hplay extends React.Component{
-	constructor(props){
+
+import loop from '../../images/loop.png'
+import previous from '../../images/previous.png'
+import play from '../../images/play.png'
+import next from '../../images/next.png'
+import list from '../../images/list.png'
+import pause from '../../images/pause.png'
+
+class Hplay extends React.Component {
+	constructor(props) {
 		super(props);
+		this.state = {
+			playstatu: false,
+			musictime: 0,
+			playtime: "00:00",
+			myaudio: './test.mp3',
+			timer: ''
+		}
+		this.play = this.play.bind(this);
 	}
-	render(){
+
+	componentDidMount() {
+		this.musictime()
+	}
+	//时间转格式
+	time(timestamp) {
+		var minutes = Math.floor(timestamp / 60);
+		var seconds = Math.floor(timestamp - (minutes * 60));
+
+		if(seconds < 10) {
+			seconds = '0' + seconds;
+		}
+		if(minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		timestamp = minutes + ':' + seconds;
+		return timestamp;
+	}
+	//总时长
+	musictime() {
+		var player = document.getElementById("audio")
+		var self = this
+		setTimeout(function() {
+			var musictime = player.duration
+			if(isNaN(musictime)) {
+				self.musictime();
+			} else {
+				self.setState({
+					musictime: self.time(player.duration)
+				})
+			}
+		}, 10);
+
+	}
+	//播放时间
+	playedtime() {
+		var player = document.getElementById("audio")
+		var self = this;
+		this.state.timer = setInterval(function() {
+			self.setState({
+				playtime: self.time(player.currentTime)
+			})
+		}, 300)
+	}
+	//进度条
+	
+	//控制播放
+	play() {
+		var player = document.getElementById("audio")
+		this.setState({
+			playstatu: !this.state.playstatu
+		})
+		if(!this.state.playstatu) {
+			player.play();
+			this.playedtime()
+		} else {
+			player.pause();
+			clearInterval(this.state.timer)
+		}
+	}
+	render() {
 		return(
 			<div id="hplay">
 			<header>
@@ -20,6 +98,7 @@ class Hplay extends React.Component{
  fenxiang"></div>
 			</header>
 			<div className="content">
+				<audio src={this.state.myaudio} id="audio"></audio>
 				<div className="pic">
 					<img src=""/>
 				</div>
@@ -39,17 +118,17 @@ class Hplay extends React.Component{
 				</ul>
 			</div>
 			<div id="time">
-				<span className="time">00:00</span>
+				<span className="time">{this.state.playtime}</span>
 				<div className="tiao"><span></span></div>
-				<span className="time">04:04</span>
+				<span className="time">{this.state.musictime}</span>
 			</div>
 			<footer>
 				<ul>
-					<li><a href="javascript:"><img src="../client/images/loop.png"/></a></li>
-					<li><a href="javascript:"><img src="../client/images/previous.png"/></a></li>
-					<li><a href="javascript:"><img src="../client/images/play.png"/></a></li>
-					<li><a href="javascript:"><img src="../client/images/next.png"/></a></li>
-					<li><a href="javascript:"><img src="../client/images/list.png"/></a></li>
+					<li><a href="javascript:"><img src={loop} className="loop"/></a></li>
+					<li><a href="javascript:"><img src={previous} className="previous"/></a></li>
+					<li><a href="javascript:"><img src={this.state.playstatu?pause:play} className="play" onClick={this.play}/></a></li>
+					<li><a href="javascript:"><img src={next} className="next"/></a></li>
+					<li><a href="javascript:"><img src={list} className="list"/></a></li>
 				</ul>
 			</footer>
 		</div>
