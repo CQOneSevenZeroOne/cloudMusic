f68b764e575a1abae77f0664f3d6983a819f8470
xiaotang66 <862115203@qq.com>
2018-02-01 11:13:04 +0800

    主页主键，评论主键

--- client/component/contatiner/xindex.jsx
+++ client/component/contatiner/xindex.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Xheader from "../index/xheader.jsx";
 import Xbanner from "../index/xbanner.jsx";
 import Xlist from "../index/xlist.jsx";
+import Xbottom from "../index/bottom.jsx";
 class Xindex extends React.Component {
 	constructor(props) {
     super(props);
@@ -12,6 +13,7 @@ class Xindex extends React.Component {
 	          <Xheader/>
 	          <Xbanner/>
 	          <Xlist/>
+	          <Xbottom/>
           </div>
     )
   }
